4f16709ddfaaa184d3a594316b40eb35bfebd60b
Garrett <gpenna@asu.edu>
2021-02-10 16:53:43 -0700

    Added functional button to clear Canvas

--- project1js.js
+++ project1js.js
@@ -1,3 +1,27 @@
+"use strict";
+
+// Global variables and Constants
+
+var canvas;
+var context;
+
+var drawingMode = false;  // drawing or moving?
+
+function initialize() {
+    canvas = document.getElementById('drawingCanvasId');
+    context = canvas.getContext('2d');
+    context.lineCap = "round";
+}
+
+function clearCanvas() {
+    //The following hard coded values are dependent on the size of canvas
+
+    var confirmClear = confirm("Do you really want to clear the canvas?");
+    if (confirmClear === true) {
+        context.clearRect(0, 0, 1000, 1000);
+    }
+}
+
 function clickLogin() {
     document.getElementById('userNameID').value='';
     document.getElementById('passwordID').value='';
@@ -6,5 +30,5 @@ function clickLogin() {
 
 function clearAfterLogin() {
     document.getElementById('inputFormID').remove();
-    document.getElementById('title').innerHTML="the Canvas";
+    document.getElementById('title').innerHTML="WebSketch";
 }
