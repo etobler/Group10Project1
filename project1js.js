9cbb33bdb063232ea8b29bae0c424a84c3817967
Emily <etobler@asu.edu>
2021-02-10 19:45:26 -0700

    adding functionality to use a pen tool

--- project1js.js
+++ project1js.js
@@ -6,11 +6,18 @@ var canvas;
 var context;
 
 var drawingMode = false;  // drawing or moving?
+var insideCanvas = false; // inside out outside canvas?
+// where the mouse was
+var previousX, previousY = 0;
+// where the mouse is 
+var currentX, currentY = 0;
 
 function initialize() {
     canvas = document.getElementById('drawingCanvasId');
     context = canvas.getContext('2d');
     context.lineCap = "round";
+    setColor('black');
+	setLineWidth('1');
 }
 
 function clearCanvas() {
@@ -31,4 +38,55 @@ function clickLogin() {
 function clearAfterLogin() {
     document.getElementById('inputFormID').remove();
     document.getElementById('title').innerHTML="WebSketch";
+}
+
+function setColor(color) {
+    if (color == "black") {
+        context.strokeStyle="#000000"
+    }
+    else {
+        context.strokeStyle="#000000"
+    }
+    // Will add more colors here when that functionality is added 
+}
+
+function setLineWidth(width) {
+    context.lineWidth = width;
+}
+
+function mouseMovement(event) {
+    // function to handle when mouse moves
+    previousX = currentX;
+    previousY = currentY;
+    currentX = event.clientX - canvas.offsetLeft;
+    currentY = event.clientY - canvas.offsetTop;
+    startDrawing()
+}
+
+function startDrawing() {
+    // Will start drawing
+    if (insideCanvas && drawingMode) {
+        context.beginPath();
+        context.moveTo(previousX, previousY);
+        context.lineTo(currentX, currentY);
+        context.stroke();
+        context.closePath();
+    }
+}
+
+function mouseEnteredCanvas(event) {
+    insideCanvas = true;
+}
+
+function mouseLeftCanvas(event) {
+    document.body.style.cursor = "help"
+    insideCanvas = false;
+}
+
+function mouseDown(event) {
+    drawingMode = true;
+}
+
+function mouseUp(event) {
+    drawingMode = false;
 }
